b0cf75fec17e600818f4344599b4ed4c5eaa0bc3
jklasd2001 <jklasd2001@naver.com>
2022-06-30 16:32:20 +0900

    feat: 절대경로 사용하도록 수정 및 path 라이브러리 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: 'src', replacement: resolve('src') }],
+  },
 })
